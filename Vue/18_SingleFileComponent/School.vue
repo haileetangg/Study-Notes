1fd12f0e05006ebf5ddd383005d6983b0a2def72
haileetangg <153844571+haileetangg@users.noreply.github.com>
2024-02-17 16:34:16 +0800

    练习Vue单文件的写法

---
+++ Vue/18_SingleFileComponent/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="demo">欢迎来到{{name}}大学学习</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'School',
+  data(){
+    return {
+      name:'西华'
+    }
+  }
+}
+</script>
+
+<style>
+  .demo{
+    color:'hotpink'
+  }
+</style>
---
+++ Vue/18_SingleFileComponent/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="demo">hello,我是{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Student',
+  data(){
+    return {
+      name:'hailee'
+    }
+  }
+}
+</script>
+
+<style>
+  .demo{
+    color:'hotpink'
+  }
+</style>
